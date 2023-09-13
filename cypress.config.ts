06cde9bd8b1761279ff84100d437c81036fd0b1f
Ryan Duffy <ryan@replay.io>
2023-09-13 08:01:32 -0700

    test upload

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,13 +15,16 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      cypressReplay.default(on, config)
+      cypressReplay.default(on, config, {
+        upload: true,
+        apiKey: process.env.REPLAY_API_KEY,
+      })
 
       on('after:run', (afterRun: any) => {
-        const data = JSON.stringify(afterRun.totalDuration);
-        const filename = "duration.json"
-        fs.writeFileSync(filename, data);
-        console.log('cypress-json-results: wrote results to %s', filename);
+        const data = JSON.stringify(afterRun.totalDuration)
+        const filename = 'duration.json'
+        fs.writeFileSync(filename, data)
+        console.log('cypress-json-results: wrote results to %s', filename)
       })
 
       return require('./cypress/plugins/index.js')(on, config)
